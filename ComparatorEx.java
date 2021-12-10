7ae8fd3b037372e6cfe986d8435cbdf9f3780ac8
Priyanka-Pruthi <40305362+Priyanka-Pruthi@users.noreply.github.com>
2021-12-10 12:19:06 +0530

    Delete ComparatorEx.java

--- ComparatorEx.java
+++
@@ -1,9 +0,0 @@
-import java.util.Comparator;
-
-public class ComparatorEx
-{
-    public int compareByRole(User user1,User user2)
-    {
-        return user2.getRole().compareTo(user1.getRole());
-    }
-}
