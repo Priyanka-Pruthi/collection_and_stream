757dbd13ec64b441a1afc9ccee6f882ae6999e0d
Priyanka-Pruthi <40305362+Priyanka-Pruthi@users.noreply.github.com>
2021-12-10 12:20:11 +0530

    Delete InsufficientBalance.java

--- InsufficientBalance.java
+++
@@ -1,7 +0,0 @@
-public class InsufficientBalance extends Exception {
-    String message = "There is insufficient balance in your Account";
-
-    public InsufficientBalance(String message) {
-        super(message);
-    }
-}
